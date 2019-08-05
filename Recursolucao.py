d384a75cae1efaff697b134e8c71d164884ca347
Mateus Costa Luz <mateus.luz01@fatec.sp.gov.br>
2019-08-05 19:25:44 -0300

    Update Recursolucao.py

--- Recursolucao.py
+++ Recursolucao.py
@@ -1,26 +1,30 @@
-﻿def asterisco(a):
-	if(a == 1):
-		print('*', end = '')
-	else:
-		print('*', end = '')
-		asterisco(a - 1)
+def asterisco(a):
+    if (a == 1):
+        print('*', end='')
+    else:
+        print('*', end='')
+        asterisco(a - 1)
+
 
 def underscore(u, a):
-	if(u == 0):
-		asterisco(a)
-	else:
-		print('_', end = '')
-		underscore(u - 1, a)
-		print('_', end = '')
+    if (u == 0):
+        asterisco(a)
+    else:
+        print('_', end='')
+        underscore(u - 1, a)
+        print('_', end='')
+
 
 def impressao(n, i):
     if n > 0:
-        underscore(n-1, i)
+        underscore(n - 1, i)
         print()
-        impressao(n-1, i+2)
+        impressao(n - 1, i + 2)
+
 
 def piramide(n):
     i = 1
     impressao(n, i)
+
 
 piramide(6)
